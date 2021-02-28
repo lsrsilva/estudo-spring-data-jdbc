9d2f82b8f7af233a496f3f13e9433b79dbbb1cfb
Lucas da Silva Rezende <lucasrezende@pactosolucoes.com.br>
2021-02-28 02:13:28 -0300

    UserRepo

--- src/main/java/com/lsr/estudospringjdbc/repositories/UserRepository.java
+++ src/main/java/com/lsr/estudospringjdbc/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.lsr.estudospringjdbc.repositories;
 import com.lsr.estudospringjdbc.entities.User;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     @Query("SELECT * FROM USER")
     List<User> testeNativeQuery();
